920be5e4b4570b07fc6da98b2dc96280911dbef8
Harsimran Singh Maan <maan.harry@gmail.com>
2024-01-06 20:58:58 +0000

    cipher done

--- decrypt.py
+++ decrypt.py
@@ -2,7 +2,7 @@ message = input("Enter your message: ")
 key = input("Enter your key: ")
 letters =  list()
 for char in message:
-    new_char = ord(char) + int(key)
+    new_char = ord(char) - int(key)
     print(char, ord(char), new_char, chr(new_char))
     letters.append(chr(new_char))
 
