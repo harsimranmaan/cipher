dc0f7cab7903bf4ed6fdbc928de432129549b9a9
Harsimran Singh Maan <maan.harry@gmail.com>
2024-01-13 21:02:27 +0000

    Updated the program

--- cipher.py
+++ cipher.py
@@ -1,10 +1,25 @@
 message = input("Enter your message: ")
 key = input("Enter your key: ")
+operation = input("operation, type D/d for decryption ")
+key = int(key) % 26
+if operation == "D" or operation == "d":
+    key = key * -1
+lower_a_val= ord('a')
+upper_a_val = ord('A')
+
 letters =  list()
 for char in message:
-    new_char = ord(char) + int(key)
-    print(char, ord(char), new_char, chr(new_char))
+    int_val = ord(char)
+    new_char = int_val
+    if int_val >= lower_a_val and int_val < lower_a_val+26:
+        new_char = ord(char) + int(key)
+        if new_char > lower_a_val+26:
+            new_char = new_char - 26
+    if int_val >= upper_a_val and int_val < upper_a_val+26:
+        new_char = ord(char) + int(key)
+        if new_char > upper_a_val + 26:
+            new_char =  new_char - 26
     letters.append(chr(new_char))
 
 phrase = ''.join(letters)
-print(message, key,phrase)
+print(message, key , phrase)
